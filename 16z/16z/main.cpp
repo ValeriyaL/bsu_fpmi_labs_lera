5c6d85c3f45737b38b0ba6558981b45c13e7b366
ValeriyaL <leraliudchyk96@gmail.com>
2015-04-08 19:19:52 +0300

    threads

---
+++ 16z/16z/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <vector> 
+
+std::mutex mutex;
+
+unsigned long long sum = 0;
+
+void summ_func(unsigned long long first, unsigned long long second, unsigned long long number)
+{
+	for (unsigned long long i = first; i <= second; i++)
+		if (!(number%i))
+			sum += i;
+}
+
+int main()
+{
+	int threads;
+	std::cout << "Please input number of threads: ";
+	std::cin >> threads;
+	unsigned long long number;
+	std::cout << "Please input your number: ";
+	std::cin >> number;
+	std::vector<std::thread> vect1;
+	unsigned long long interval = (number / 2) / threads;
+	unsigned long long begin = 0;
+	auto start_time = std::chrono::system_clock::now();
+	for (int j = 0; j < threads; j++)
+	{
+		begin++;
+		vect1.push_back(std::thread(summ_func, begin, begin + interval, number));
+		begin += interval;
+	}
+	for (int j = 0; j < threads; j++)
+		vect1[j].join();
+	auto end_time = std::chrono::system_clock::now();
+	auto dif = end_time - start_time;
+	if (number == sum)
+		std::cout << "Your number is perfect" << std::endl;
+	else
+		std::cout << "Your number isn't perfect" << std::endl;
+	std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::milliseconds>(dif).count() << std::endl;
+	return 0;
+}
