cbae56f34845d499bf66b0361abfc1ec3c198f41
ValeriyaL <leraliudchyk96@gmail.com>
2015-04-23 19:13:53 +0300

    2nd

--- 18var2/18var2/main.cpp
+++ 18var2/18var2/main.cpp
@@ -3,32 +3,27 @@
 using namespace std;
 int main()
 {
-	int number, one=1, two=2;
+	int number, one=1, two=2, rad;
 	bool is_simple = 1;
 	cout << "Please input your number: ";
 	cin >> number;
 	__asm
 	{
-		mov eax, number
-		cmp eax,2
-		je ex
-		mul one
-		div two
-		mov ecx, eax
-		dec ecx
-		mov esi,1
-	mark1:
-		inc esi
+		mov ecx,1
+	start:
+		inc ecx
+		mov eax,ecx 
+		mul eax
+		cmp eax,number 
+		jg ex
 		mov eax,number
-		mov edx,0
-		div esi
+		div ecx
 		cmp edx,0
 		je not_simple
-		loop mark1
-		jmp ex
+		jmp start
 	not_simple:
 		mov is_simple, 0
-	ex:
+	ex :
 	}
 	cout << is_simple << endl;
 	return 0;
