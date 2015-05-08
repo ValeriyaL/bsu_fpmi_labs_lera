89e7f2257c9454f47f62888f2d7467a8eaf34cb9
ValeriyaL <leraliudchyk96@gmail.com>
2015-05-08 16:45:16 +0300

    polynom

--- 19.2/19.2/main.cpp
+++ 19.2/19.2/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	int size1, size2, summ_size, mult_size;
+	int size1, size2, mult_size = 1, summ_size;
 	/*cout << "Please input number of coefficients of your first polynom: ";
 	cin >> size1;
 	cout << "Please input coefficient of your first polynom: ";
@@ -15,109 +15,96 @@ int main()
 	int polyn2[10000];
 	for (int i = 0; i < size2; i++)
 	cin >> polyn2[i];*/
-	int polyn1[100] = { 1, -2, 3, -1 };
+	int polyn1[100] = { 1, -10, 3, -1 };
 	int polyn2[100] = { 1, 8, -1, 4, 5 };
 	size1 = 4;
 	size2 = 5;
-	int summ[10000];
-	int mult[10000];
+	int summ[10000] = { 0, };
+	int mult[10000] = { 0, };
 	__asm
 	{
 		//summ
 		mov eax, size1
-			mov ecx, size2
-			cmp eax, ecx
-			jg second
-			mov summ_size, ecx
-			mov ecx, size2
-			xor ebx, ebx
-		cycle :
-		xor edx, edx
-			mov edx, polyn2[ebx * 4]
-			cmp eax, 0
-			je c1
-			add edx, polyn1[ebx * 4]
-			dec eax
-		c1 :
-		mov summ[ebx * 4], edx
-			inc ebx
-			loop cycle
-			jmp ex
-		second :
+		mov ecx, size2
+		cmp eax, ecx
+		jg first
+		mov summ_size, ecx
+		jmp sum
+	first :
 		mov summ_size, eax
-			mov ecx, size1
-			mov eax, size2
-			xor ebx, ebx
-		cycle2 :
-		xor edx, edx
-			mov edx, polyn1[ebx * 4]
-			cmp eax, 0
-			je c2
-			add edx, polyn2[ebx * 4]
-			dec eax
-		c2 :
-		mov summ[ebx * 4], edx
-			inc ebx
-			loop cycle2
-		ex :
-		//mult
+	sum :
 		cmp size1, 0
-			je null1
-			cmp size2, 0
-			je null1
-			mov eax, size1
-			dec eax
-			add eax, size2
-			mov mult_size, eax
-			mov ebx, size1
-		zer1 :
-		cmp ebx, mult_size
-			je zer2
-			mov polyn1[ebx * 4], 0;
+		je second
+		mov ecx, size1
+		xor ebx, ebx
+	pol1 :
+		mov eax, polyn1[ebx * 4]
+		mov summ[ebx * 4], eax
 		inc ebx
-			jmp zer1
-		zer2 :
+		loop pol1
+	second :
+		cmp size2, 0
+		je end1
+		mov ecx, size2
+		xor ebx, ebx
+	pol2 :
+		mov eax, polyn2[ebx * 4]
+		add summ[ebx * 4], eax
+		inc ebx
+		loop pol2
+	end1 :
+		cmp summ_size, 0
+		jne sec_z
+		mov summ_size, 1
+		//mult
+	sec_z :
+		cmp size1, 0
+		je exit1
+		cmp size2, 0
+		je exit1
+		mov eax, size1
+		dec eax
+		add eax, size2
+		mov mult_size, eax
+		mov ebx, size1
+	zer1 :
+		cmp ebx, mult_size
+		je zer2
+		mov polyn1[ebx * 4], 0;
+		inc ebx
+		jmp zer1
+	zer2 :
 		mov ebx, size2
-		zerr :
+	zerr :
 		cmp ebx, mult_size
-			je zer3
-			mov polyn2[ebx * 4], 0;
+		je multip
+		mov polyn2[ebx * 4], 0;
 		inc ebx
-			jmp zerr
-		zer3 :
-		mov ecx, mult_size
-			xor esi, esi
-		zer :
-		mov mult[esi * 4], 0
-			inc esi
-			loop zer
-
-			xor edi, edi
-		mult1 :
+		jmp zerr
+	multip :
+		//mult[k]=mult[k]+first[i]*second[k-i];
+		xor edi, edi
+	mult1 :
 		xor esi, esi
-		mult2 :
+	mult2 :
 		mov eax, polyn1[esi * 4]
-			mov ebx, edi
-			sub ebx, esi
-			xor edx, edx
-			mov ecx, polyn2[ebx * 4]
-			imul ecx
-			add mult[edi * 4], eax
-			inc esi
-			cmp esi, edi
-			jg vneshn
-			jmp mult2
-		vneshn :
+		mov ebx, edi
+		sub ebx, esi
+		xor edx, edx
+		mov ecx, polyn2[ebx * 4]
+		imul ecx
+		add mult[edi * 4], eax
+		inc esi
+		cmp esi, edi
+		jg vneshn
+		jmp mult2
+	vneshn :
 		inc edi
-			cmp edi, mult_size
-			jg exit1
-			jmp mult1
-			jmp exit1
-		null1 :
-		xor eax, eax
-			mov mult[eax], 0
-			mov mult_size, 1
-		exit1 :
+		cmp edi, mult_size
+		jg exit1
+		jmp mult1
+		jmp exit1
+	exit1 :
 	}
 	for (int i = 0; i < summ_size; i++)
 		cout << summ[i] << " ";
